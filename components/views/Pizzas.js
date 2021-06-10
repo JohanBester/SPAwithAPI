2469f3887ed0b607ea48a6ec55e34a897d4b2369
Johan Bester <johan.bester@yahoo.com>
2021-06-09 21:33:10 -0500

    Update pizza page

--- components/views/Pizzas.js
+++ components/views/Pizzas.js
@@ -1,11 +1,11 @@
 export default (st) => `
 <table id="pizzas">
-<tr><th>Crust</th><th>Cheese</th><th>Sauce</th><th>Toppings</th></tr>
+<tr><th>Size</th><th>Crust</th><th>Cheese</th><th>Sauce</th><th>Toppings</th></tr>
 ${st.pizzas
   .map((pizza) => {
-    return `<tr><td>${pizza.crust}</td><td>${pizza.cheese}</td><td>${
-      pizza.sauce
-    }</td><td>${pizza.toppings.join(" & ")}</td></tr>`;
+    return `<tr><td>${pizza.size}</td><td>${pizza.crust}</td><td>${
+      pizza.cheese
+    }</td><td>${pizza.sauce}</td><td>${pizza.toppings.join(" & ")}</td></tr>`;
   })
   .join("")}
 </table>
