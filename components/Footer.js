7b1d2b47af79e4bfa225bdd4d914cdda711f4e40
Johan Bester <johan.bester@yahoo.com>
2021-11-10 17:35:27 -0600

    Update Packages

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 import html from "html-literal";
 import * as state from "../store";
 
-const kelvinToFahrenheit = kelvinTemp =>
+const kelvinToFahrenheit = (kelvinTemp) =>
   Math.round((kelvinTemp - 273.15) * (9 / 5) + 32);
 
 let city = state.Home.weather.name;
@@ -10,6 +10,11 @@ let feelLike = kelvinToFahrenheit(parseInt(state.Home.weather.feelsLike));
 
 export default () => html`
   <footer>
+    <h3>
+      Weather in ${state.Home.weather.city}
+      ${kelvinToFahrenheit(state.Home.weather.temp)}F, feels like
+      ${kelvinToFahrenheit(state.Home.weather.feelsLike)}F
+    </h3>
     &copy; 2021 <a href="https://savvycoders.com/">Savvy Coders</a>
   </footer>
 `;
